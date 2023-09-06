3c2485417262c78d1a4e94952b24dd581277e088
RuiG <rui.nf1@gmail.com>
2023-09-06 10:41:24 +0200

    binary search

--- algos/binary-search.c
+++ algos/binary-search.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-#define TRUE 0
-#define FALSE 1
+#include <math.h>
 
 typedef struct Array Array;
 
@@ -13,32 +10,51 @@ struct Array
     int values[];
 };
 
-int linearSearch(Array *h, int n)
+int binarySearch(int v[], size_t len, int n)
 {
-    for (size_t i = 0; i < h->length; ++i)
+    size_t l = 0;
+    size_t h = len;
+    size_t m;
+    do
     {
-        if (h->values[i] == n)
-        {
-            return i;
+        m = (size_t)floor(l + (h - l) / 2);
+
+        printf("l: %lu, m: %lu, h: %lu -> v[m]: %d \n", l, m, h, v[m]);
+        
+        if (v[m] == n)
+        {   
+            return v[m];
         }
+        else if (v[m] < n)
+        {
+            l = m + 1;
+        }
+        else
+        {
+            h = m;
+        };
+    } while (l < h);
+
+    return -1; // not found
+};
+
+int binarySearchArray(Array *h, int n)
+{
+    if (h == NULL)
+    {
+        return -1;
     }
 
-    return -1;
-}
-
-int randomGen(int min, int max)
-{
-    return rand() % (max - min + 1) + min;
+    return binarySearch(h->values, h->length, n);
 }
 
 int main()
 {
-    srand(time(NULL));
     system("clear");
 
     size_t *length;
     int *input_n;
-    
+
     printf("How big of an array? ");
     scanf("%lu", length);
 
@@ -53,15 +69,15 @@ int main()
         arr->length = *length;
 
         // fill
-        printf("Array: { ");
+        printf("Initial Array: { ");
         for (size_t i = 0; i < *length; i++)
         {
-            arr->values[i] = randomGen(1, 25);
+            arr->values[i] = i;
             printf("%d ", arr->values[i]);
         }
         printf("}\n");
 
-        int result = linearSearch(arr, *input_n);
+        int result = binarySearchArray(arr, *input_n);
         printf("Result: %d\n", result);
 
         free(arr); // 🫡
